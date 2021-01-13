c624ea24ca2b7227e29abd5a10981761ff90d96c
Dekpo Yologaza <dekpo@me.com>
2021-01-13 12:19:41 +0100

    Added a side menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,6 +10,34 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
   styleUrls: ['app.component.scss']
 })
 export class AppComponent {
+
+  public selectedIndex = 0;
+
+  public appPages = [
+    {
+      title: 'Home',
+      url: '/tabs/tab1',
+      icon: 'home'
+    },
+    {
+      title: 'Photos',
+      url: '/tabs/tab2',
+      icon: 'images'
+    },
+    {
+      title: 'Contact',
+      url: '/tabs/tab3',
+      icon: 'person'
+    },
+    {
+      title: 'Mail',
+      url: '/tabs/tab4',
+      icon: 'mail'
+    }
+  ];
+
+
+
   constructor(
     private platform: Platform,
     private splashScreen: SplashScreen,
